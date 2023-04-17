f8a130757400db70cf95cb8eccef54795141987e
Radosław Drab <radoslaw.drab03@gmail.com>
2023-04-17 21:35:28 +0200

    added random key generator function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -16,3 +16,17 @@ export function saveStateToLocalStorage(state: State) {
 	const encrypted = CryptoJS.AES.encrypt(stateStr, decryptionKey)
 	localStorage.setItem(appLSKey, encrypted.toString())
 }
+
+export function generateRandomKey(length: number = 10): { number: number; string: string } {
+	const digitsCount: number = Math.max(length, 1)
+	const digits: number[] = []
+
+	for (let i = 0; i < digitsCount; i++) {
+		const randomDigit = Math.round(Math.random() * 9)
+		digits.push(randomDigit)
+	}
+	const key = digits.reduce((acc, digit) => {
+		return (acc += digit)
+	}, '')
+	return { number: +key, string: key }
+}
