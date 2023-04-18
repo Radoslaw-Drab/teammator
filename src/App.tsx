82a1b8e9cd901173e217ccd2f335484715ac648e
Radosław Drab <radoslaw.drab03@gmail.com>
2023-04-18 23:26:03 +0200

    implemented context wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import React from 'react'
-
 import Header from 'components/Header/Header'
 import Main from 'components/Main/Main'
+import AppContextWrapper from 'utils/AppContextWrapper'
 
 function App() {
 	return (
-		<>
+		<AppContextWrapper>
 			<Header />
 			<Main />
-		</>
+		</AppContextWrapper>
 	)
 }
 
