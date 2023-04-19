b45acd4fe8c295b4362a059ab8f1cfafa77c4102
Radosław Drab <radoslaw.drab03@gmail.com>
2023-04-19 15:31:08 +0200

    simplified and removed unnecessary code

--- src/components/FormContent/GroupCountInput.tsx
+++ src/components/FormContent/GroupCountInput.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from 'react'
 import useAppContext from 'utils/use-app-context'
 
 import Button from 'components/UI/Button/Button'
@@ -8,18 +7,13 @@ import styles from './GroupCountInput.module.scss'
 
 function GroupCountInput() {
 	const { state, dispatch } = useAppContext()
-	const [count, setCount] = useState(clampWithin(state.groupsCount))
 
 	function increase() {
-		setCount((value) => clampWithin((value += 1)))
+		dispatch({ type: 'SET_GROUP_COUNT', groupsCount: clampWithin(state.groupsCount + 1) })
 	}
 	function decrease() {
-		setCount((value) => clampWithin((value -= 1)))
+		dispatch({ type: 'SET_GROUP_COUNT', groupsCount: clampWithin(state.groupsCount - 1) })
 	}
-	useEffect(() => {
-		dispatch({ type: 'SET_GROUP_COUNT', groupsCount: clampWithin(count) })
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [count])
 	function clampWithin(value: number) {
 		return Math.min(Math.max(value, 2), Math.max(state.people.length, 2))
 	}
@@ -33,7 +27,7 @@ function GroupCountInput() {
 				<Button onClick={increase}>
 					<PlusIcon />
 				</Button>
-				<span>{count}</span>
+				<span>{state.groupsCount}</span>
 				<Button onClick={decrease}>
 					<MinusIcon />
 				</Button>
