47c983f75704249c674b9770ce045ac4a101498f
Radosław Drab <radoslaw.drab03@gmail.com>
2023-04-22 22:00:48 +0200

    added footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import Header from 'components/Header/Header'
 import Main from 'components/Main/Main'
+import Footer from 'components/Footer/Footer'
 import AppContextWrapper from 'utils/AppContextWrapper'
 import AppSettingsContextWrapper from 'utils/AppSettingsContextWrapper'
 
 function App() {
 	return (
 		<AppSettingsContextWrapper>
-		<AppContextWrapper>
-			<Header />
-			<Main />
-		</AppContextWrapper>
+			<AppContextWrapper>
+				<Header />
+				<Main />
+				<Footer />
+			</AppContextWrapper>
 		</AppSettingsContextWrapper>
 	)
 }
