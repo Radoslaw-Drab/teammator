c4ac0fba72a16eca4701d2065bea73f979bf1f6c
Radosław Drab <radoslaw.drab03@gmail.com>
2023-04-22 15:56:10 +0200

    added settings context wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import Header from 'components/Header/Header'
 import Main from 'components/Main/Main'
 import AppContextWrapper from 'utils/AppContextWrapper'
+import SettingsContextWrapper from 'utils/SettingsContextWrapper'
 
 function App() {
 	return (
+		<SettingsContextWrapper>
 		<AppContextWrapper>
 			<Header />
 			<Main />
 		</AppContextWrapper>
+		</SettingsContextWrapper>
 	)
 }
 
